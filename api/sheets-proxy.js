269c6be14f4c93d60260812fdab5a4cfdd9f33d6
ikiw <163365900+echoshil@users.noreply.github.com>
2025-10-17 10:00:39 +0700

    Update sheets-proxy.js

--- api/sheets-proxy.js
+++ api/sheets-proxy.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method === "OPTIONS") return res.status(200).end();
 
   try {
-    const scriptUrl = "https://script.google.com/macros/s/AKfycbw0zYejxmsStcpMY0vepcMRXq_UUEC2vkzuOdv91TKJ0dcZgSwm-CfpPO2vI9p6Xm_m/exec"; // 🔹 Ganti dengan URL kamu
+    const scriptUrl = "https://script.google.com/macros/s/AKfycbyTU9yet5oTOTtaKGqaiuW8oAcusar96MXujNkM3Mt3MklKuqcP0wpUwy6D77urMxvI/exec"; // 🔹 Ganti dengan URL kamu
     const response = await fetch(scriptUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -19,5 +19,6 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: error.toString() });
   }
 }
+
 
 
